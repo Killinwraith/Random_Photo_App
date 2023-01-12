1b36c8b5b054bb8ded250921c2936e8ea98d5ff4
Hetang Mehta <112903225+Killinwraith@users.noreply.github.com>
2023-01-11 18:00:07 -0700

    Second Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Hetang Mehta on 2023-01-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
